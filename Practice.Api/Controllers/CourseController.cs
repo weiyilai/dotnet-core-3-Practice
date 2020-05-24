ae2c4a61b9f05316e2466c4d6d6da19150d37b8c
Weiyi Lai <joy777park@gmail.com>
2020-05-24 15:56:32 +0800

    修改 api return 加入 summary

--- Practice.Api/Controllers/CourseController.cs
+++ Practice.Api/Controllers/CourseController.cs
@@ -13,6 +13,9 @@ using Microsoft.AspNetCore.Http;
 
 namespace Practice.Api.Controllers
 {
+    /// <summary>
+    /// Course Api
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class CourseController : ControllerBase
@@ -25,6 +28,10 @@ namespace Practice.Api.Controllers
         }
 
         // GET: api/<CourseController>
+        /// <summary>
+        /// 取得所有Course資料
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public async Task<IActionResult> Get()
         {
@@ -33,7 +40,12 @@ namespace Practice.Api.Controllers
         }
 
         // GET api/<CourseController>/5
-        [HttpGet("{id}")]
+        /// <summary>
+        /// 取得指定的Course資料
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id:int}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesDefaultResponseType]
@@ -50,6 +62,11 @@ namespace Practice.Api.Controllers
         }
 
         // POST api/<CourseController>
+        /// <summary>
+        /// 新增Course資料
+        /// </summary>
+        /// <param name="course"></param>
+        /// <returns></returns>
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesDefaultResponseType]
@@ -62,11 +79,19 @@ namespace Practice.Api.Controllers
                 DepartmentID = 1
             };
             _courseRepository.Add<Course>(t);
-            return Created("/api/Course/" + t.CourseID, t);
+            return CreatedAtAction(nameof(Get), new { id = t.CourseID }, t);
         }
 
         // PUT api/<CourseController>/5
-        [HttpPut("{id}")]
+        /// <summary>
+        /// 更新Course資料
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="course"></param>
+        /// <returns></returns>
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesDefaultResponseType]
         public async Task<IActionResult> Put(int id, [FromBody] CourseDTO course)
         {
             var t = await _courseRepository.GetSingleAsync<Course>(t => t.CourseID == id);
@@ -74,17 +99,24 @@ namespace Practice.Api.Controllers
             t.Credits = course.Credits;
             _courseRepository.Update<Course>(t);
 
-            return Ok();
+            return NoContent();
         }
 
         // DELETE api/<CourseController>/5
+        /// <summary>
+        /// 刪除指定的Course資料
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesDefaultResponseType]
         public async Task<IActionResult> Delete(int id)
         {
             var t = await _courseRepository.GetSingleAsync<Course>(t => t.CourseID == id);
             _courseRepository.Delete<Course>(t);
 
-            return Ok();
+            return NoContent();
         }
     }
 }
